7125df26859c5f9db591054626bc6db06a8265fa
高伟 <gaowei232@crc.com.hk>
2025-08-24 20:16:54 +0800

    vite基础原理

---
+++ apps/frontend/vite/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
+const app = express();
+const port = 3000;
+
+// 中间件设计 AOP
+app.get('/',(req,res,next) =>{
+    res.sendFile(path.resolve(__dirname,'./index.html'))
+})
+
+
+// 需要注意 express 版本
+app.get(/^\/(.*)\.js$/,(req,res)=>{
+    const reqPath = req.path;
+    const file = fs.readFileSync(path.resolve(__dirname,`.${reqPath}`),'utf-8');
+
+
+    // TODO:处理逻辑
+    // 1. 编译处理（esbuild\swc\babel\rollup）
+    // 2. 插件处理
+    // 3. 产物处理、压缩、混淆
+
+
+
+    res.type('js')
+    res.send(file);
+   // res.send('Miracle Hello')
+})
+ 
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+})
---
+++ apps/frontend/vite/src/main.js
@@ -0,0 +1,10 @@
+
+
+
+function setStyle(){
+    document.title = "Vite First";
+
+    document.body.style.backgroundColor = '#c590c5';
+}
+
+setStyle();
